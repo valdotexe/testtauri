1983722395c4c2b19cef54bb06882414b07e568e
val.exe <valantis.alatsas@gmail.com>
2024-05-30 21:39:22 +0300

    added backend placeholers

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,9 +7,27 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn insert(name: &str) -> Result<String, String> {
+    // TODO: actually insert name into database
+    Ok("success".into())
+}
+
+#[tauri::command]
+fn select() -> Vec<String> {
+    let mut name_vec = Vec::new();
+    // TODO: Read names from database
+    // for now returning placeholder names:
+    name_vec.push("val.exe".to_string());
+    name_vec.push("sperdev".to_string());
+    name_vec.push("blackassasin4".to_string());
+    name_vec
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![insert])
+        .invoke_handler(tauri::generate_handler![select])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.js
+++ src/main.js
@@ -4,15 +4,15 @@ let greetInputEl;
 let greetMsgEl;
 
 async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
+    greetInputEl = document.querySelector("#greet-input");
+    greetMsgEl = document.querySelector("#greet-msg");
+    document.querySelector("#greet-form").addEventListener("submit", (e) => {
+        e.preventDefault();
+        greet();
+    });
 });
