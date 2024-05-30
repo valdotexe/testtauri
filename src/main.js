4ce22fb793f9e49d16bbacab003d8c0bb04cfab0
sperdev <gasproas@gmail.com>
2024-05-30 22:01:43 +0300

    lol

--- src/main.js
+++ src/main.js
@@ -1,18 +1,26 @@
-const { invoke } = window.__TAURI__.tauri;
+const save_button = document.getElementById("save_button");
+const name_list = document.getElementById("name_list");
+const input_name = document.getElementById("input_name");
+const namesDiv = document.getElementById("name");
 
-let greetInputEl;
-let greetMsgEl;
+var names = [];
 
-async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
-}
-
-window.addEventListener("DOMContentLoaded", () => {
-    greetInputEl = document.querySelector("#greet-input");
-    greetMsgEl = document.querySelector("#greet-msg");
-    document.querySelector("#greet-form").addEventListener("submit", (e) => {
-        e.preventDefault();
-        greet();
-    });
+save_button.addEventListener("click", () => {
+    const name = input_name.value.trim();
+    if (name){
+        names.push(name);
+        input_name.value = "";
+        alert("name saved");
+    } else {
+        alert("enter a name");
+    }
 });
+
+name_list.addEventListener("click", () =>{
+    if (names.length > 0) {
+        namesDiv.innerHTML = "<h2>saved names: </h2><ul>" + names.map(name => "<li>${name}</li>").join("") + "</ul>";
+     } else {
+        namesDiv.innerHTML = "<h2>no names saved yet</h2>"
+    }
+});
+
