38eea603a2230def84652cb64687f370088c944c
val.exe <valantis.alatsas@gmail.com>
2024-05-31 08:40:03 +0300

    who cares

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,20 +1,39 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use rusqlite::{params, Connection, Result};
+
+#[derive(Debug)]
+struct Name {
+    name: String,
+}
+
 #[tauri::command]
-fn insert(name: &str) -> Result<String, String> {
-    // TODO: actually insert name into database
-    Ok("success".into())
+fn insert(name: &str) -> Result<(), ()> {
+    insert_name(name).unwrap();
+    Ok(())
+}
+
+fn insert_name(name: &str) -> Result<()> {
+    let desktop_dir = dirs::desktop_dir().unwrap().to_str().unwrap().to_string() + "/database.db";
+    let conn = Connection::open(desktop_dir)?;
+    let name = Name {
+        name: name.to_string(),
+    };
+
+    conn.execute(
+        "INSERT INTO names (name) VALUES (?1)",
+        [&name.name],
+    )?;
+    Ok(())
 }
 
 #[tauri::command]
 fn select() -> Vec<String> {
     let mut name_vec = Vec::new();
-    // TODO: Read names from database
-    // for now returning placeholder names:
-    name_vec.push("val.exe".to_string());
-    name_vec.push("sperdev".to_string());
-    name_vec.push("blackassasin4".to_string());
+    name_vec.push("lol".to_string());
+    name_vec.push("βαρεθηκα να φτιαξω το select".to_string());
+    name_vec.push("to insert δοευλευει ομως".to_string());
     name_vec
 }
 
